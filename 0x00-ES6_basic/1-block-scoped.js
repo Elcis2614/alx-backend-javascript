3a45a6a6cbd9cb0a13f744a74c96ce350ff4a802
elcis2614 <elisereflorbusole@gmail.com>
2023-09-26 15:57:17 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task;
-  let task2;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    var task = true;
+    var task2 = false;
   }
-  task = false;
-  task2 = true;
+
   return [task, task2];
 }
