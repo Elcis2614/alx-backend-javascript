7e170fa63a2798e4fcea507e0e017dbf966eabec
elcis2614 <elisereflorbusole@gmail.com>
2023-09-28 02:36:35 +0000

    then series

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromAPI(promise) {
   promise
     .then(() => ({ status: 200, body: 'success' }))
     .then(() => { console.log('Got a response from the API'); })
-    .catch(() => (new Error()));
+    .catch(() => (new Error('')));
 }
