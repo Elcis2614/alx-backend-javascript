d45581656f0fe6eb198580b420a4683a05f78cd4
elcis2614 <elisereflorbusole@gmail.com>
2023-09-30 01:04:38 +0000

    creatIterator

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   const iterator = [];
-  for (const dpt of report.allEmployees) {
+  for (const dpt of Object.keys(report.allEmployees)) {
     iterator.push(report.allEmployees[dpt]);
   }
   return iterator;
