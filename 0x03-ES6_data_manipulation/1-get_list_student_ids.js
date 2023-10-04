1c141453fe686e4ba0d4197d3756b0f0456dfdf2
elcis2614 <elisereflorbusole@gmail.com>
2023-10-04 02:18:57 +0000

    get student list

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(arr) {
-  if (arr.constructor !== Array) return [];
+  if (arr.constructor !== Array) return new Array();
   const ids = arr.map((obj) => obj.id);
   return ids;
 }
