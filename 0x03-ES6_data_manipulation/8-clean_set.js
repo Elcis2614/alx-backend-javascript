20fda5e5a486cc0c4d6d858ef3ae4a4518f24fe0
elcis2614 <elisereflorbusole@gmail.com>
2023-10-04 02:47:27 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const chain = [];
-  if (startString === '' || typeof startString !== 'string') return '';
+  if (startString === '' || typeof startString !== 'string' || set.constructor !== Set) return '';
   set.forEach((str) => {
     if (str.indexOf(startString) === 0) chain.push(str.replace(startString, ''));
   });
