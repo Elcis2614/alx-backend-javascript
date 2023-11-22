d0fb5296c30eff0eee2f5226a0d8ca2c3d4a794c
elcis2614 <elisereflorbusole@gmail.com>
2023-11-22 01:52:39 +0000

    reading the std input

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  readline.close();
+});
+
+process.on('exit', () => console.log('This important software is now closing'));
