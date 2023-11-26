97738f1bfe8135f078f397239a198d8bec87e6a8
elcis2614 <elisereflorbusole@gmail.com>
2023-11-26 16:53:38 +0000

    expected to work

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,16 +3,20 @@ const fs = require('node:fs');
 module.exports = function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8').split('\n').slice(1);
-    console.log(`Number of students: ${data.length - 1}`);
     const mDict = {};
+    let count = 0;
     data.forEach((element) => {
-      const line = element.split(',');
-      if (line[3] in mDict) {
-        mDict[line[3]].push(line[0]);
-      } else {
-        mDict[line[3]] = [line[0]];
+      if (element !== '') {
+        const line = element.split(',');
+        if (line[3] in mDict) {
+          mDict[line[3]].push(line[0]);
+        } else {
+          mDict[line[3]] = [line[0]];
+        }
+        count += 1;
       }
     });
+    console.log(`Number of students: ${count}`);
     for (const key in mDict) {
       if (key !== 'undefined') {
         console.log(`Number of students in ${key}: ${mDict[key].length}. List: ${mDict[key].join(', ')}`);
